3eff65f9f88acc544cbf966f4a3fb97915eec1e7
Mohit <mohit.shah41@gmail.com>
2014-03-29 21:20:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+var express = require('express'),
+	http = require('http'),
+	app = express(),
+    sio = require('socket.io'), io,
+	murmurhash = require('murmurhash');
+
+/* Keep a global list of currently connected clients */
+var game = {},
+	socketPlayers = {};
+
+
+app.set('port', 8080);
+
+app.enable('strict routing');
+app.use('/game/', express.static(__dirname + '/game'));
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+io = sio.listen(server, { log: false });
+
+function send(msg, to, data) {
+	// Check that the client has already joined successfully,
+	// then foward the message to client
+	if (socketPlayers[to] !== undefined) {
+		socketPlayers[to].emit(msg, data);
+	}
+}
+
+
+/* Listen for and handle socket.io connections */
+io.sockets.on("connection", function(socket) {
+	/* Handle requests to join the chat-room */
+	socket.on('gencode', function(info, callback) {
+		var code = info.code;
+		if(code === null) {
+			var time = new Date();
+			var id = parseInt(murmurhash.v2(time.toString() + " - " + time.getMilliseconds()), 10);
+			game[id] = [socket.id];
+			socket.game = id;
+			socketPlayers[socket.id] = socket;
+			callback(id, socket.id);
+		} else if(game[code] !== null) {
+			if(game[code].length == 2) {
+				callback(false);
+			} else {
+				game[code].push(socket.id);
+				socket.game = code;
+				socketPlayers[socket.id] = socket;
+				callback(code, socket.id);
+			}
+		} else {
+			callback(false);
+		}
+	});
+
+	/* move player */
+	socket.on("moveplayer", function(info) {
+		if(game[info.gameid]!=null && game[info.gameid].length > 1) {
+			if(game[info.gameid][0] === info.id) {
+				id = game[info.gameid][1];
+			} else {
+				id = game[info.gameid][0];
+			}
+			send("moveplayer", id, info);
+		}
+	});
+
+	/* move player */
+	socket.on("moveball", function(info) {
+		if(game[info.gameid]!=null && game[info.gameid].length > 1) {
+			if(game[info.gameid][0] === info.id) {
+				id = game[info.gameid][1];
+			} else {
+				id = game[info.gameid][0];
+			}
+			send("moveball", id, info);
+		}
+	});
+
+	/* Handle client disconnection */
+	socket.on("disconnect", function() {
+		// Check that the user has already joined successfully
+		if (socket.id) {
+			delete game[socket.game];						// delete user info from global list
+			delete socketPlayers[socket.id];							// delete socket info from global list
+			//io.sockets.emit("userDisconnected", socket.user);	// Let all the remaining clients know of the disconnect
+		}
+	});
+});
+
---
+++ game/Main.js
@@ -0,0 +1,325 @@
+$(function(){
+	var socket = io.connect("10.25.168.32:8080");
+	var code = null, id = null, youserver = 1;
+	if(window.location.hash !== "") {
+		var code = window.location.hash.slice(1);
+		youserver=0;
+	}
+	socket.emit('gencode', { code : code }, function(c, i) {
+		if(code === null) {
+			var div = $("#link");
+			div.html("Invite another player to : " + window.location.href + "#" + c);
+		}
+		code = c;
+		id = i;
+	});
+	canvas=document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+
+	W=800;
+	H=600;
+	
+	var ballX;
+	var ballY;
+	var ballOffsetW;
+	var ballOffsetH;
+	var player1={};
+	var player2={};
+	var flag = 0;
+	var dx=0;
+	var dy=0;
+	var speed=3;
+	var startBtn = {};
+	var restartBtn = {};
+	var over = 0;
+	var dx=dy=speed;
+	var join_second_player=0;
+	
+	var img = new Image;
+	img.src = "ball.png";
+	var bg_img=new Image;
+	bg_img.src="bg.jpg";
+	var p1_img=new Image;
+	p1_img.src="r.png";
+	var p2_img=new Image;
+	p2_img.src="l.png";
+		
+	function init(){
+	
+	ballX=W-64-25-25;
+	ballY=120;
+	ballOffsetW=24;
+	ballOffsetH=24;
+	player1={};
+	player2={};
+	fps = 60;
+	flag = 0;
+	dx=0;
+	dy=0;
+	speed=3;
+	startBtn = {};
+	restartBtn = {};
+	over = 0;
+	dx=dy=speed;
+	join_second_player=0;
+	
+	var img = new Image;
+	img.src = "ball.png";
+	var bg_img=new Image;
+	bg_img.src="bg.jpg";
+	var p1_img=new Image;
+	p1_img.src="r.png";
+	var p2_img=new Image;
+	p2_img.src="l.png";
+		player1['h']=64;
+		player1['w']=64;
+		player1['x']=W-player1['w']-25;
+		player1['y']=100;
+
+		player2['h']=64;
+		player2['w']=64;
+		player2['x']=25;
+		player2['y']=H-170;
+	}
+	// Function to paint paintCanvas
+	function paintCanvas() {
+	ctx.drawImage(bg_img,3,3);
+		ctx.beginPath();
+      ctx.rect(0,0,W,H);
+      ctx.lineWidth = 7;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+      ctx.lineWidth = 4;
+      ctx.moveTo(W/2,0);
+      ctx.lineTo(W/2,H);
+      ctx.stroke();
+      ctx.moveTo(0,100);
+      ctx.lineTo(W,100);
+      ctx.stroke();
+      ctx.moveTo(0,H-100);
+      ctx.lineTo(W,H-100);
+      ctx.stroke();
+      ctx.moveTo(200,100);
+      ctx.lineTo(200,H-100);
+      ctx.stroke();
+      ctx.moveTo(W-200,100);
+      ctx.lineTo(W-200,H-100);
+      ctx.stroke();
+      ctx.moveTo(200,H/2);
+      ctx.lineTo(W-200,H/2);
+      ctx.stroke();
+	}
+
+	// Draw everything on canvas
+	function draw() {
+			paintCanvas();	
+			player1draw();
+			player2draw();
+			balldraw();	
+	}
+
+	function player1draw(){
+		ctx.drawImage(p1_img,player1['x'], player1['y'],64,64);
+	}
+
+	function player2draw(){
+		ctx.drawImage(p2_img,player2['x'], player2['y'],64,64);
+	}
+
+	function balldraw(){
+		ctx.drawImage(img,ballX,ballY,24,24);
+	}
+
+	function start(){
+		setInterval(function ()
+		{
+		   // MOVE BALL
+		   ballX += dx;
+		   ballY += dy;
+
+		   // See if ball is past player paddle
+
+		   if(ballX < 0)
+		   {
+		   	alert("you loose");
+		      
+		      init();
+		   }
+
+		   // See if ball is past CPU paddle
+
+		   if((ballX + ballOffsetW) > W)
+		   {
+		      alert("Second player win");
+		      init();
+		   }
+
+		   // COLLISION DETECTION
+
+		   // If ball hits upper or lower wall
+		   if(ballY < 0 || ((ballY + ballOffsetH) > H))
+		      dy = -dy; // Make y direction opposite
+
+		   // If ball hits player paddle
+
+		   	if(((ballY + ballOffsetH) > player1['y']) && ballY < (player1['y'] + player1['h']) && ballX+ballOffsetW > (player1['x'])){
+		         dx = -dx;
+		     }
+		    
+
+		   
+		   	if(((ballY+ballOffsetH) > player2['y']) && ballY < (player2['y'] + player2['h']) && (ballX) < player2['x']+player2['w']){
+		         dx = -dx;
+		   	}
+		      
+		   // Place ball at calculated positions
+		   data={};
+		   data['x']=ballX;
+		   data['y']=ballY;
+		   data['dx']=dx;
+		   data['dy']=dy;
+		   moveBallPosition(data);
+		   draw();
+		},10);
+	}
+
+	var control_player;
+	if(youserver==1){
+	control_player = player1;
+	}
+	else{
+	control_player = player2;
+	}
+
+	var controller = new Leap.Controller();
+    var start_flag=0;
+    var start_frame='';
+    var start_frame_id='';
+    var i=0;
+    var prev=0;
+    var prevx=0;
+    controller.on( 'frame' , function(frame){
+
+      if(frame.hands.length==1){
+      		if(frame.hands[0].fingers.length==1){
+      			finger=frame.hands[0].fingers[0];
+
+      			currentPosition = finger.tipPosition;
+      		
+      				paddle=control_player;
+      				
+      				if(youserver==1){
+						if(Math.abs(currentPosition[1]-prev)>2 || Math.abs(currentPosition[0]-prevx)>2){
+												
+							if(currentPosition[1]-prev>0){
+								if(paddle['y']-10>0){
+									paddle['y']=paddle['y']-10;
+								}
+							}
+							else{
+								if(paddle['y']+10+paddle['h']<H){
+									paddle['y']=paddle['y']+10;
+								}
+							}
+
+							if(currentPosition[0]-prevx>0){
+								if(paddle['x']+64<W-2){
+									paddle['x']=paddle['x']+10;
+								}
+							}
+							else{
+								if(paddle['x']-5>W/2){
+									paddle['x']=paddle['x']-10;
+								}
+							}
+							prev=currentPosition[1];
+							prevx=currentPosition[0];	
+							
+	      					control_player=paddle;
+	      					moveMyPosition(control_player);
+							draw();
+						}
+					}
+					else{
+						if(Math.abs(currentPosition[1]-prev)>2 || Math.abs(currentPosition[0]-prevx)>2){
+												
+							if(currentPosition[1]-prev>0){
+								if(paddle['y']-10>0){
+									paddle['y']=paddle['y']-10;
+								}
+							}
+							else{
+								if(paddle['y']+10+paddle['h']<H){
+									paddle['y']=paddle['y']+10;
+								}
+							}
+
+							if(currentPosition[0]-prevx>0){
+								if(paddle['x']+5<W/2){
+									paddle['x']=paddle['x']+10;
+								}
+							}
+							else{
+								if(paddle['x']-64>2){
+									paddle['x']=paddle['x']-10;
+								}
+							}
+							prev=currentPosition[1];
+							prevx=currentPosition[0];	
+							
+	      					control_player=paddle;
+	    					moveMyPosition(control_player);  					
+							draw();
+						}	
+					}
+      		}
+      }
+	});
+	controller.connect();
+	img.onload=function(){
+		init();
+		draw();
+	};
+	/*if(youserver==1){
+		start();
+	}*/
+	
+	socket.on("moveball", function(data) {
+		if(youserver==1){
+		
+		}
+		else{
+			ballX=data['x'];
+			ballY=data['y'];
+			dx=data['dx'];
+			dy=data['dy'];
+			draw();
+		}
+		
+	});
+
+	function moveMyPosition(position) {
+		var data = $.extend(position, { gameid: ""+code, id: id });
+		socket.emit("moveplayer", data);
+	}
+
+	function moveBallPosition(position) {
+		var data = $.extend(position, { gameid: ""+code, id: id });
+		socket.emit("moveball", data);
+	}
+
+	socket.on("moveplayer", function(data) {
+		
+		if(youserver==1){
+			if(join_second_player==0){
+				start();
+				join_second_player=1;
+			}
+			player2=data;
+		}
+		else{
+			player1=data;
+		}
+		draw();
+	});
+});
